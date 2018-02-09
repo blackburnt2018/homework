9b3d727fb11f01c83c503a650e0131ba5d9c6b22
blackburnt2018 <tblackburn@sjnma.org>
2018-02-09 09:42:13 -0600

    homework

---
+++ hw3/p1.py
@@ -0,0 +1,15 @@
+first_name = raw_input("what is your first name?")
+last_name = raw_input ("what is your last name?")
+full_name = first_name +" "+ last_name
+print(full_name)
+
+
+number1 = 22
+number2 = 64.0
+number3 = number1 + number2
+print(number3)
+
+
+
+
+TodayIsThursday = True
