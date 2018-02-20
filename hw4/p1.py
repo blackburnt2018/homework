7a8e820303f2fb04412a92c239abf5e636d66662
blackburnt2018 <tblackburn@sjnma.org>
2018-02-20 14:41:36 -0600

    homework

---
+++ hw4/p1.py
@@ -0,0 +1,5 @@
+s = raw_input("please enter a sentence")
+words = s.split()
+
+sentence = "\n ".join(words)
+print(sentence)
---
+++ hw4/p2.py
@@ -0,0 +1,6 @@
+s = raw_input("please enter your full name")
+upper_s = s.upper()
+print(upper_s)
+
+
+
