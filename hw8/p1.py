2f2f0bdc5717a239bb659750c90f659c792c2282
blackburnt2018 <tblackburn@sjnma.org>
2018-04-13 09:20:42 -0500

    homework

---
+++ hw8/p1.py
@@ -0,0 +1,26 @@
+import random
+
+rollresult1 = 0
+rollresult2 = 0
+score1 = 0
+score2 = 0
+rollrange = [1,6]
+ledger = []
+
+
+def roll(beginning,end):
+	random.randint
+
+def determineWinnerRound(rollresult1,rollresult2):
+
+
+
+
+
+
+
+
+def determineWinnerGame(score1,score2):
+
+
+
